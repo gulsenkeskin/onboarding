ac592fbd3b1c3aa8e12e290f52763ee139fa9cdf
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-29 18:45:33 +0300

    Update onboarding.dart

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -10,10 +10,24 @@ class OnboardingPage extends StatefulWidget {
 class _OnboardingPageState extends State<OnboardingPage> {
   @override
   Widget build(BuildContext context)=> Scaffold(
-    body: PageView(
-      children: [
-
-      ],
+    body: Container(
+      padding: const EdgeInsets.only(bottom: 80),
+      child: PageView(
+        children: [
+          Container(
+            color: Colors.red,
+            child: const Center(child: Text("1"),),
+          ),
+          Container(
+            color: Colors.indigo,
+            child: const Center(child: Text("2"),),
+          ),
+          Container(
+            color: Colors.green,
+            child: const Center(child: Text("3"),),
+          ),
+        ],
+      ),
     ),
   );
 }
