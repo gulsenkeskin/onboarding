12ecfc0ea772dcf8edb4535571b3c7e66f523a4d
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-29 22:43:46 +0300

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:onboarding/onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>Scaffold(
-    appBar: AppBar(
-      title: const Text('Home Page'),
-    ),
-    body: const Center(
-      child: Text('Home', style: TextStyle(color:  Colors.black),),
-    ),
-  );
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          actions: [
+            IconButton(
+                onPressed: () async {
+                  final prefs = await SharedPreferences.getInstance();
+                  prefs.setBool('showHome', false);
+
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => const OnboardingPage()));
+                },
+                icon: const Icon(Icons.logout))
+          ],
+        ),
+        body: const Center(
+          child: Text(
+            'Home',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      );
 }
